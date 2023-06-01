b049f70b841f89fb3e650b660139e60b28cec9a5
Ivan <duremar98@yandex.ru>
2023-06-02 01:29:31 +0300

    header_style

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 
-import { City, Content, Insta, Phone } from "./style";
+import { Block, Content } from "./style";
+import { ReactComponent as SvgPhone } from "../../assets/phone.svg";
+import { ReactComponent as SvgCity } from "../../assets/city.svg";
+import { ReactComponent as SvgInsta } from "../../assets/insta.svg";
 
 const Header = () => {
   return (
     <Content>
-      <Phone>1</Phone>
-      <City>2</City>
-      <Insta>3</Insta>
+      <Block cg={"12"}>
+        <SvgPhone />
+        <span>+375 29 292-29-29 (VEL)</span>
+        <span>+375 33 333-33-33 (МТС)</span>
+      </Block>
+      <Block cg={"8"}>
+        <SvgCity />
+        <span>ваш город</span>
+      </Block>
+      <Block cg={"15"}>
+        <SvgInsta />
+        <span>посетите нашу страницу</span>
+      </Block>
     </Content>
   );
 };
--- src/components/header/style.tsx
+++ src/components/header/style.tsx
@@ -11,8 +11,34 @@ export const Content = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-around;
+  font-weight: 500;
+  div:not(:first-child) {
+    cursor: pointer;
+  }
 `;
 
-export const Phone = styled.div``;
-export const City = styled.div``;
-export const Insta = styled.div``;
+export const Block = styled.div<{ cg: string }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: ${({ cg }) => `${cg}px`};
+  font-size: 15px;
+
+  ${(props) =>
+    props.cg === "12" &&
+    `
+    span{
+        cursor: pointer;
+    }
+    `}
+
+  ${(props) =>
+    props.cg === "15" &&
+    `
+    font-size:14px;
+    span{
+        max-width: 150px;
+    }
+   
+    `}
+`;
