1b64a5dd214ddc153a9efb7ebed126d043703531
Ivan <duremar98@yandex.ru>
2023-06-01 23:58:52 +0300

    +

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import { City, Content, Insta, Phone } from "./style";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Content>
+      <Phone>1</Phone>
+      <City>2</City>
+      <Insta>3</Insta>
+    </Content>
+  );
 };
 
 export default Header;
--- src/components/header/style.tsx
+++ src/components/header/style.tsx
@@ -1,3 +1,18 @@
 import styled from "styled-components";
 
-const Content = styled.div``;
+export const Content = styled.header`
+  height: 77px;
+  background: linear-gradient(
+    269.91deg,
+    rgba(46, 49, 146, 0.7) 0%,
+    rgba(212, 20, 90, 0.7) 98.16%
+  );
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const Phone = styled.div``;
+export const City = styled.div``;
+export const Insta = styled.div``;
