3ad085282c8573ff4d39d57b4fac359612ed16f8
Ivan <duremar98@yandex.ru>
2023-06-08 23:49:54 +0300

    +

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { Content, Blocks } from "./style";
+import { Content, Blocks, Block } from "./style";
+import { ReactComponent as SvgFooterInsta } from "../../assets/footerInsta.svg";
 
 const Footer = () => {
   return (
     <Content>
-      <Blocks>123</Blocks>
+      <Blocks>
+        <Block>
+          <h3>МЫ В СОЦИАЛЬНЫХ СЕТЯХ</h3>
+          <SvgFooterInsta />
+        </Block>
+        <Block>
+          <h3>КОНТАКТНЫЕ ТЕЛЕФОНЫ</h3>
+          <p>
+            +375 29 292-29-29 (VEL) <br />
+            +375 33 333-33-33 (МТС)
+          </p>
+        </Block>
+        <Block>
+          <h3>О НАС</h3>
+          <p>Индивидуальное изготовление EVA ковриков для вашего автомобиля</p>
+          <p>ваш город</p>
+        </Block>
+      </Blocks>
     </Content>
   );
 };
--- src/components/footer/style.tsx
+++ src/components/footer/style.tsx
@@ -13,4 +13,25 @@ export const Blocks = styled.div`
   max-width: 935px;
   width: 100%;
   border: 1px solid red;
+  display: flex;
+  justify-content: space-between;
+`;
+export const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+  h3 {
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 16px;
+    text-align: left;
+  }
+  p {
+    max-width: 224px;
+    font-size: 14px;
+    line-height: 16px;
+  }
+  svg {
+    cursor: pointer;
+  }
 `;
